13acc12c5f9d3a524f874fa0d68e20422872f5ec
guthember <guthember@gmail.com>
2020-12-02 21:39:30 +0100

    Hegy osztály létrehozása

---
+++ HegyekMo/Hegy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HegyekMo
+{
+  class Hegy
+  {
+    private string nev;
+
+    public string Nev
+    {
+      get { return nev; }
+    }
+
+    private string hegyseg;
+
+    public string Hegyseg
+    {
+      get { return hegyseg; }
+    }
+
+    private int magassag;
+
+    public int Magassag
+    {
+      get { return magassag; }
+    }
+
+    public Hegy(string nev, string hegyseg, int magassag)
+    {
+      this.nev = nev;
+      this.hegyseg = hegyseg;
+      this.magassag = magassag;
+    }
+  }
+}
