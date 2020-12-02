edc03f6afd04f5403486588b72f77371c941e071
guthember <guthember@gmail.com>
2020-12-02 21:48:41 +0100

    Adatok beolvasása és listában eltárolása

--- HegyekMo/Program.cs
+++ HegyekMo/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HegyekMo
 {
   class Program
   {
+    static List<Hegy> hegyek = new List<Hegy>();
+
+    static void Beolvasas()
+    {
+      StreamReader be = new StreamReader("hegyekMO.txt");
+
+      be.ReadLine();
+
+      while (!be.EndOfStream)
+      {
+        string[] a = be.ReadLine().Split(';');
+        hegyek.Add(new Hegy(a[0], a[1], int.Parse(a[2])));
+      }
+
+      be.Close();
+    }
+
     static void Main(string[] args)
     {
+      Beolvasas();
+
+
+      Console.ReadLine();
     }
   }
 }
