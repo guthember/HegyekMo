1ed8d01d477e62f710a8e9e593f8d9481d73d04c
guthember <guthember@gmail.com>
2020-12-03 10:22:21 +0100

    Vezérlők elehelyezése

--- HegyekForm/Form1.Designer.cs
+++ HegyekForm/Form1.Designer.cs
@@ -29,13 +29,141 @@ namespace HegyekForm
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.btnHarom = new System.Windows.Forms.Button();
+      this.btnNegyedik = new System.Windows.Forms.Button();
+      this.btnOtodik = new System.Windows.Forms.Button();
+      this.btnHatodik = new System.Windows.Forms.Button();
+      this.btnHetedik = new System.Windows.Forms.Button();
+      this.btnNyolcadik = new System.Windows.Forms.Button();
+      this.btnKilencedik = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.tbMagassag = new System.Windows.Forms.TextBox();
+      this.lbKimenet = new System.Windows.Forms.ListBox();
+      this.SuspendLayout();
+      // 
+      // btnHarom
+      // 
+      this.btnHarom.Location = new System.Drawing.Point(12, 12);
+      this.btnHarom.Name = "btnHarom";
+      this.btnHarom.Size = new System.Drawing.Size(75, 23);
+      this.btnHarom.TabIndex = 0;
+      this.btnHarom.Text = "3. feladat";
+      this.btnHarom.UseVisualStyleBackColor = true;
+      // 
+      // btnNegyedik
+      // 
+      this.btnNegyedik.Location = new System.Drawing.Point(12, 41);
+      this.btnNegyedik.Name = "btnNegyedik";
+      this.btnNegyedik.Size = new System.Drawing.Size(75, 23);
+      this.btnNegyedik.TabIndex = 1;
+      this.btnNegyedik.Text = "4. feladat";
+      this.btnNegyedik.UseVisualStyleBackColor = true;
+      // 
+      // btnOtodik
+      // 
+      this.btnOtodik.Location = new System.Drawing.Point(12, 70);
+      this.btnOtodik.Name = "btnOtodik";
+      this.btnOtodik.Size = new System.Drawing.Size(75, 23);
+      this.btnOtodik.TabIndex = 2;
+      this.btnOtodik.Text = "5. feladat";
+      this.btnOtodik.UseVisualStyleBackColor = true;
+      // 
+      // btnHatodik
+      // 
+      this.btnHatodik.Location = new System.Drawing.Point(12, 99);
+      this.btnHatodik.Name = "btnHatodik";
+      this.btnHatodik.Size = new System.Drawing.Size(75, 23);
+      this.btnHatodik.TabIndex = 3;
+      this.btnHatodik.Text = "6. feladat";
+      this.btnHatodik.UseVisualStyleBackColor = true;
+      // 
+      // btnHetedik
+      // 
+      this.btnHetedik.Location = new System.Drawing.Point(12, 128);
+      this.btnHetedik.Name = "btnHetedik";
+      this.btnHetedik.Size = new System.Drawing.Size(75, 23);
+      this.btnHetedik.TabIndex = 4;
+      this.btnHetedik.Text = "7. feladat";
+      this.btnHetedik.UseVisualStyleBackColor = true;
+      // 
+      // btnNyolcadik
+      // 
+      this.btnNyolcadik.Location = new System.Drawing.Point(12, 157);
+      this.btnNyolcadik.Name = "btnNyolcadik";
+      this.btnNyolcadik.Size = new System.Drawing.Size(75, 23);
+      this.btnNyolcadik.TabIndex = 5;
+      this.btnNyolcadik.Text = "8. feladat";
+      this.btnNyolcadik.UseVisualStyleBackColor = true;
+      // 
+      // btnKilencedik
+      // 
+      this.btnKilencedik.Location = new System.Drawing.Point(12, 186);
+      this.btnKilencedik.Name = "btnKilencedik";
+      this.btnKilencedik.Size = new System.Drawing.Size(75, 23);
+      this.btnKilencedik.TabIndex = 6;
+      this.btnKilencedik.Text = "9. feladat";
+      this.btnKilencedik.UseVisualStyleBackColor = true;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(105, 90);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(106, 13);
+      this.label1.TabIndex = 7;
+      this.label1.Text = "Magasság méterben:";
+      // 
+      // tbMagassag
+      // 
+      this.tbMagassag.Location = new System.Drawing.Point(108, 102);
+      this.tbMagassag.Name = "tbMagassag";
+      this.tbMagassag.Size = new System.Drawing.Size(100, 20);
+      this.tbMagassag.TabIndex = 8;
+      // 
+      // lbKimenet
+      // 
+      this.lbKimenet.FormattingEnabled = true;
+      this.lbKimenet.Location = new System.Drawing.Point(230, 8);
+      this.lbKimenet.Name = "lbKimenet";
+      this.lbKimenet.Size = new System.Drawing.Size(558, 433);
+      this.lbKimenet.TabIndex = 9;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
+      this.Controls.Add(this.lbKimenet);
+      this.Controls.Add(this.tbMagassag);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.btnKilencedik);
+      this.Controls.Add(this.btnNyolcadik);
+      this.Controls.Add(this.btnHetedik);
+      this.Controls.Add(this.btnHatodik);
+      this.Controls.Add(this.btnOtodik);
+      this.Controls.Add(this.btnNegyedik);
+      this.Controls.Add(this.btnHarom);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Magyarország hegyei";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Button btnHarom;
+    private System.Windows.Forms.Button btnNegyedik;
+    private System.Windows.Forms.Button btnOtodik;
+    private System.Windows.Forms.Button btnHatodik;
+    private System.Windows.Forms.Button btnHetedik;
+    private System.Windows.Forms.Button btnNyolcadik;
+    private System.Windows.Forms.Button btnKilencedik;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox tbMagassag;
+    private System.Windows.Forms.ListBox lbKimenet;
   }
 }
 
