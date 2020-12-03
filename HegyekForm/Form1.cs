c13f62189904186e3e7f8007d6e592a0cfcd280e
guthember <guthember@gmail.com>
2020-12-03 10:08:03 +0100

    Hegy osztály felokosítása

--- HegyekForm/Form1.cs
+++ HegyekForm/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace HegyekForm
 {
   public partial class Form1 : Form
   {
+    List<Hegy> hegyek = new List<Hegy>();
+    
+
+    #region Beolvasás
+    void Beolvasas()
+    {
+      StreamReader be = new StreamReader("hegyekMO.txt");
+
+      be.ReadLine();
+
+      while (!be.EndOfStream)
+      {
+        string[] a = be.ReadLine().Split(';');
+        hegyek.Add(new Hegy(a[0], a[1], int.Parse(a[2])));
+      }
+
+      be.Close();
+    }
+    #endregion
+
     public Form1()
     {
       InitializeComponent();
+      Beolvasas();
     }
   }
 }
--- HegyekForm/Hegy.cs
+++ HegyekForm/Hegy.cs
@@ -8,6 +8,7 @@ namespace HegyekForm
 {
   class Hegy
   {
+    private const double LAB = 3.280839895;
 
     private string nev;
 
@@ -30,11 +31,27 @@ namespace HegyekForm
       get { return magassag; }
     }
 
+    private double labertek;
+
+    public double Labertek
+    {
+      get { return labertek; }
+    }
+
+    private string labSzoveg;
+
+    public string LabSzoveg
+    {
+      get { return labSzoveg; }
+    }
+
     public Hegy(string nev, string hegyseg, int magassag)
     {
       this.nev = nev;
       this.hegyseg = hegyseg;
       this.magassag = magassag;
+      labertek = magassag * LAB;
+      labSzoveg = Math.Round(labertek, 1).ToString().Replace(',', '.');
     }
   }
 }
